a20747a27fe226d2ba9f060ec3588feba1fcb0a5
PreetThakkar-04 <57306972+PreetThakkar-04@users.noreply.github.com>
2020-02-28 23:45:10 +0530

    Update home.js

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -6,9 +6,9 @@ class Home extends Component {
     return(
       <div className="home-wrap">
         <div className="landing">
-          <h1 className="home-title">SoNIC</h1>
+          <h1 className="home-title" style="background-color:white; ">SoNIC</h1>
             <div className="land-desc col-lg-6 col-md-7">
-                <a href="/auth/google" style="color:red">Login</a> with Google.
+                <a href="/auth/google" style="color:red; background-color:white;">Login</a> with Google.
             </div>
         </div>
       </div>
