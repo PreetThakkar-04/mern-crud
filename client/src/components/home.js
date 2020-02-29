ad4226ca3f22b79f1233b823d33dabc06cbbfb41
PreetThakkar-04 <57306972+PreetThakkar-04@users.noreply.github.com>
2020-02-29 05:56:56 +0530

    Update home.js

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -7,28 +7,6 @@ class Home extends Component {
       <div className="home-wrap">
         <div className="landing">
           <h1 className="home-title">SoNIC</h1>
-              <div className="login-home">
-                  <form>
-                          <div>
-                          <label>Email  </label>
-                          <input
-                              type="email"
-                              name="email"
-                          />
-                          </div>
-                          <div>
-                              <label>Password</label>
-                              <input
-                                  type="password"
-                                  name="password"
-                              />
-                          </div>
-                 </form>
-              </div>
-              <div classname="button-home">
-                              <input type="submit" value = "Login"/>
-                              <input type="submit" value = "Signup"/>
-              </div>
               <div className="land-desc col-lg-6 col-md-7">
                     <a href="/auth/google">Login</a> with Google.
               </div>
